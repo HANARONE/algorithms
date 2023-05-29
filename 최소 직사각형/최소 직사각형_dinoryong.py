d1e379c023a930adcbe7fc10c797e6baf0cbc487
Dinoryong <biiregrey2@gmail.com>
2023-05-29 22:03:26 +0900

    Update

---
+++ 최소 직사각형/최소 직사각형_dinoryong.py
@@ -0,0 +1,19 @@
+def solution(sizes):
+    answer = 0
+    temp = []
+    temp2 = []
+
+    # 2차원 배열 sort할 경우 결과는
+    # sizes.sort(reverse=True)
+    # print(sizes)
+
+    # 2차원 배열에서 주의 . max도 첫번째원소에만 먹힌다
+    # print(max(sizes))
+
+    for i in range(len(sizes)):
+        temp.append(max(sizes[i]))
+        temp2.append(min(sizes[i]))
+
+    answer = max(temp) * max(temp2)
+
+    return answer
