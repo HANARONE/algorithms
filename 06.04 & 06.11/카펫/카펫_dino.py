8fb4fdef4d39cb6489411fc0ab89b1f025edd107
Dinoryong <youngzi.kooo@gmail.com>
2023-06-11 20:11:57 +0900

    Update 카펫_dino.py

--- 06.04 & 06.11/카펫/카펫_dino.py
+++ 06.04 & 06.11/카펫/카펫_dino.py
@@ -1,11 +1,17 @@
-def solution(brown, yellow):
-    answer = []
-    total = brown + yellow
-    for b in range(1, total+1):
-        if (total / b) % 1 == 0:
-            a = total / b
-            if a >= b:
-                if (2*a) + (2*b) == (brown + 4):
-                    return [a, b]
+'''sketch
+종이에 그려봤음.
 
-    return answer
+'''
+
+def solution(brown, yellow):
+    answer = [0, 0]
+    total = brown + yellow
+    # w >= h 니까, w 부터 생각하고 h를 구하면서 순회, 큰수부터 작은수로.
+    for w in range(total, 2, -1):
+        #  w로 나누어 떨어지는 h 구하고,
+        if total % w == 0:
+            h = total // w
+            # 테두리 길이인 2 만큼 뺸 후의 넓이가 yellow의 면적과 같으면 문제 조건 성립. 해당 값 바로 return해서 빠져나가기
+            if yellow == (w-2) * (h-2):
+                answer = [w, h]
+                return answer
