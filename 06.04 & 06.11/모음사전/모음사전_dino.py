7e560f97f9c8f9ec938de4bc96947f96dcc0677b
Dinoryong <youngzi.kooo@gmail.com>
2023-06-11 20:45:14 +0900

    Update 모음사전_dino.py

--- 06.04 & 06.11/모음사전/모음사전_dino.py
+++ 06.04 & 06.11/모음사전/모음사전_dino.py
@@ -1,11 +1,19 @@
+# 중복순열 가져온 풀이
 from itertools import product
-
-
 def solution(word):
     answer = 0
     dictionary = []
     for i in range(1, 6):
-        dictionary.extend(
-            list(map(''.join, product(['A', 'E', 'I', 'O', 'U'], repeat=i))))
+        dictionary.extend(list(map(''.join, product(['A','E','I','O','U'], repeat=i))))
     dictionary.sort()
     return dictionary.index(word)+1
+
+# A : 1, E : 782, I : 1563번째 단어, 
+def solution(word):
+    answer = 0
+    arr = ['A', 'E', 'I', 'O', 'U']
+    num = [781, 156, 31, 6, 1]
+    for i in range(len(word)):
+        answer += arr.index(word[i]) * num[i] + 1
+    
+    return answer
