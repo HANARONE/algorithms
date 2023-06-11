47046c27672392fa7726d0f0bbd907fc6100ca95
Dinoryong <youngzi.kooo@gmail.com>
2023-06-11 20:49:54 +0900

    Update 가장 큰 수_dino.py

--- 06.04 & 06.11/가장 큰 수/가장 큰 수_dino.py
+++ 06.04 & 06.11/가장 큰 수/가장 큰 수_dino.py
@@ -1,4 +1,5 @@
 def solution(numbers):
     numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3, reverse=True)
+    # 람다함수로 한 줄로 해봤다
+    numbers.sort(key = lambda x: x*3, reverse=True)
     return str(int(''.join(numbers)))
