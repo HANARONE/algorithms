c2fe5102e2e950536f70fa69a5d84a2271155ea7
Dinoryong <youngzi.kooo@gmail.com>
2023-06-11 20:48:31 +0900

    Update k번째수_dino.py

--- 06.04 & 06.11/K번째 수/k번째수_dino.py
+++ 06.04 & 06.11/K번째 수/k번째수_dino.py
@@ -4,7 +4,10 @@ def solution(array, commands):
         i = command[0]
         j = command[1]
         k = command[2]
+        # 조건대로 자르기
         arr = array[i-1:j]
+        # 정렬해주고
         arr.sort()
+        # k번째 숫자 골라서 배열에 추가해주기
         answer.append(arr[k-1])
     return answer
