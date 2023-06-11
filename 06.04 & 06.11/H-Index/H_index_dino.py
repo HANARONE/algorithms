7246f4a710ec473184d914a2cd9f6c4becb665e1
Dinoryong <youngzi.kooo@gmail.com>
2023-06-11 20:54:01 +0900

    Update H_index_dino.py

--- 06.04 & 06.11/H-Index/H_index_dino.py
+++ 06.04 & 06.11/H-Index/H_index_dino.py
@@ -1,6 +1,11 @@
 def solution(citations):
+    # 정렬해주고
     citations.sort()
+    # 다 순회 , 완전탐색하는데
     for i in range(len(citations)):
+        # 조건에 맞다면
         if citations[i] >= len(citations) - i:
             return len(citations) - i
+        
+    # 위에거를 다 스루했다면 0 return
     return 0
