05e49ae22f2a542f37abbf5346a926ca56c170f6
Dinoryong <youngzi.kooo@gmail.com>
2023-06-11 20:37:14 +0900

    Update 연속된 부분 수열의 합_dino.py

--- 06.04 & 06.11/연속된 부분 수열의 합/연속된 부분 수열의 합_dino.py
+++ 06.04 & 06.11/연속된 부분 수열의 합/연속된 부분 수열의 합_dino.py
@@ -1,13 +1,15 @@
+# 처음에 엄청 복잡하게 생각했다가 투 포인터인데 다른 방법도 있을 듯 훔 
+
 def solution(sequence, k):
     answer = []
     right = 0
-    cnt = 0
-
+    cnt = 0    
+    
     for left in range(len(sequence)):
         while right < len(sequence) and cnt < k:
             cnt += sequence[right]
             right += 1
-
+        
         if cnt == k:
             if not answer:
                 answer = [left, right-1]
@@ -16,7 +18,7 @@ def solution(sequence, k):
                     answer = [left, right-1]
                 else:
                     pass
-
-        cnt -= sequence[left]
-
+        
+        cnt -= sequence[left]        
+    
     return answer
