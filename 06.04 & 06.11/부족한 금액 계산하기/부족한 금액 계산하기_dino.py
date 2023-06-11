e556b583023a2d5249a7c8984d8f5fc3fd284f07
Dinoryong <youngzi.kooo@gmail.com>
2023-06-11 20:55:55 +0900

    Update 부족한 금액 계산하기_dino.py

--- 06.04 & 06.11/부족한 금액 계산하기/부족한 금액 계산하기_dino.py
+++ 06.04 & 06.11/부족한 금액 계산하기/부족한 금액 계산하기_dino.py
@@ -1,12 +1,15 @@
 def solution(price, money, count):
     result = 0
-
+    
+    # 다 순회해본다. 완전 탐색
     for i in range(count):
+        # 남은 돈 세기
         money = money - (price * (i+1))
-
+        
     if money >= 0:
         result = 0
     else:
+        # 얼마 모자르는지
         result = -(money)
-
+    
     return result
