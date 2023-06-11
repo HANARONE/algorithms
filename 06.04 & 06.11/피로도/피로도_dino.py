001dd58cf5a1af057e947c25b01d58501da3754f
Dinoryong <youngzi.kooo@gmail.com>
2023-06-11 20:28:13 +0900

    Update 피로도_dino.py

--- 06.04 & 06.11/피로도/피로도_dino.py
+++ 06.04 & 06.11/피로도/피로도_dino.py
@@ -1,20 +1,22 @@
-''' 
-[[80,20].[50,40],[30,10]]
 
-'''
+# 순열 가져오고
 from itertools import permutations
 
 
 def solution(k, dungeons):
-    answer = -1
+    # return answer 변수 초기화
+    answer = 0
+    # 순열로 모든 case를 cases 배열로 저장한다
     cases = list(permutations(dungeons, len(dungeons)))
+    # case 하나씩 완탐 돌리며너 탐험할 수 있는 던전 수 count
     for case in cases:
-        hp, count = k, 0
+        temp, cnt = k, 0
         for need, consume in case:
-            if hp >= need:
-                hp -= consume
-                count += 1
-            else:
-                break
-        answer = max(answer, count)
+            # 필요한 피로도보다 남아있는 피로도가 많다면 해당 던전에 갈 수 있다.
+            if temp >= need:
+                temp -= consume
+                cnt += 1
+                
+        answer = max(answer, cnt)
+        
     return answer
