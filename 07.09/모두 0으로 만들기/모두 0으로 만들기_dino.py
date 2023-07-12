9d1f6a6e420ea8142035cc35721dc8dc2ec07372
Dinoryong <biiregrey3@gmail.com>
2023-07-12 16:08:38 +0900

    update

--- 07.09/모두 0으로 만들기/모두 0으로 만들기_dino.py
+++ 07.09/모두 0으로 만들기/모두 0으로 만들기_dino.py
@@ -1,26 +1,6 @@
-from collections import defaultdict
 
-answer = 0
 
 
 def solution(a, edges):
-    if sum(a) != 0:
-        return -1
 
-    graph = defaultdict(list)
-    for u, v in edgesL
-        graph[u].append(v)
-        graph[v].append(u)
-
-    def dfs(u, v):
-        global answer
-        for node in graph[u]:
-            if node != v:
-                dfs(node, u)
-        answer += abs(a[u])
-        a[v] += a[u]
-        a[u] = 0
-
-    dfs(0, 0)
-    
     return answer
